a5e15ed30d0424c018cc62f021b8bf7d817ead0d
Paran-oid <AzizHmid10000@gmail.com>
2024-04-05 21:23:51 +0100

    git fix?

--- MyEmployee/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MyEmployee/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -133,7 +133,6 @@ namespace MyEmployee.Areas.Identity.Pages.Account
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
