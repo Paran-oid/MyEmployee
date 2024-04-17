fb766e73b76736bb3455b51016cb7999eb056b8a
Paran-oid <AzizHmid10000@gmail.com>
2024-04-17 11:40:27 +0100

    Phone implementation

---
+++ MyEmployee/wwwroot/js/Phone/i18n/el/interface.mjs
@@ -0,0 +1,10 @@
+//* Translated by: Anthony Veaudry (anthony0030).
+export default {
+  selectedCountryAriaLabel: "Επιλεγμένη χώρα",
+  noCountrySelected: "Δεν έχει επιλεγεί χώρα",
+  countryListAriaLabel: "Κατάλογος χωρών",
+  searchPlaceholder: "Αναζήτηση",
+  zeroSearchResults: "Δεν βρέθηκαν αποτελέσματα",
+  oneSearchResult: "Βρέθηκε 1 αποτέλεσμα",
+  multipleSearchResults: "Βρέθηκαν ${count} αποτελέσματα",
+};
